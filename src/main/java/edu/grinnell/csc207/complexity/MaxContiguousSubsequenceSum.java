4d6c6f8d4e46249471d5546cb86384c2908cb4a5
houston0206 <puffdiddie@gmail.com>
2026-02-06 11:06:17 -0600

    Friday Lab

--- src/main/java/edu/grinnell/csc207/complexity/MaxContiguousSubsequenceSum.java
+++ src/main/java/edu/grinnell/csc207/complexity/MaxContiguousSubsequenceSum.java
@@ -4,6 +4,9 @@ import java.util.Arrays;
 import java.util.Random;
 
 public class MaxContiguousSubsequenceSum {
+
+	public static int count = 0;
+
 	public static int compute1(int[] arr) {
 		int max = 0;
 		for (int i = 0; i < arr.length; i++) {
@@ -11,11 +14,12 @@ public class MaxContiguousSubsequenceSum {
 				int sum = 0;
 				for (int k = i; k <= j ; k++) {
 					sum += arr[k];
+					count++;
 				}
 				max = Math.max(max, sum);
 			}
 		}
-		return max;
+		return count;
 	}
 	
 	public static int compute2(int[] arr) {
@@ -24,10 +28,11 @@ public class MaxContiguousSubsequenceSum {
 			int sum = 0;
 			for (int j = i; j < arr.length; j++) {
 				sum += arr[j];
+				count++;
 				max = Math.max(max, sum);
 			}
 		}
-		return max;
+		return count;
 	}
 
 	public static int compute3(int[] arr) {
@@ -35,9 +40,10 @@ public class MaxContiguousSubsequenceSum {
 		int sum = 0;
 		for (int i = 0; i < arr.length; i++) {
 			sum = Math.max(0, arr[i] + sum);
+			count++;
 			max = Math.max(sum, max);
 		}
-		return max;
+		return count;
 	}
 	
 	public static int[] generateRandomArray(Random rand, int n, int k) {
@@ -51,14 +57,14 @@ public class MaxContiguousSubsequenceSum {
 	
 	public static void main(String[] args) {
 		Random rand = new Random();
-		int size = 10;
-		int range = 10;
+		int size = 100000;
+		int range = 100000;
 		System.out.print(String.format("Generating a random array of size %d... ", size));
 		int[] arr = generateRandomArray(rand, size, range);
 		System.out.println("complete!");
-		System.out.println(String.format("arr = %s", Arrays.toString(arr)));
+		//System.out.println(String.format("arr = %s", Arrays.toString(arr)));
 		System.out.println(String.format("compute1(arr) = %d", compute1(arr)));
-		System.out.println(String.format("compute2(arr) = %d", compute2(arr)));
-		System.out.println(String.format("compute3(arr) = %d", compute3(arr)));
+		//System.out.println(String.format("compute2(arr) = %d", compute2(arr)));
+		//System.out.println(String.format("compute3(arr) = %d", compute3(arr)));
 	}
 }
